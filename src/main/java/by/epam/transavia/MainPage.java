b0823defa09749ca85cab2398ac353fa60ebcea7
IvanPalubatonau <Ivan.Palubatonau@gmail.com>
2017-08-05 22:07:04 +0200

    first test case is completed

--- src/main/java/by/epam/transavia/MainPage.java
+++ src/main/java/by/epam/transavia/MainPage.java
@@ -1,7 +1,5 @@
 package by.epam.transavia;
 
-
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -11,40 +9,115 @@ import org.openqa.selenium.support.PageFactory;
 public class MainPage extends BasePage {
 	public static final String URL = "https://www.transavia.com/en-UK/home/";
 
-	@FindBy(id = ".//*[@id='desktop']/section/div[1]/h1")
+	@FindBy(xpath = "//*[@id='desktop']/section/div[1]/h1")
 	private WebElement inscriptionOfPage;
 
-	@FindBy(id = ".//*[@id='routeSelection_DepartureStation-input']")
+	@FindBy(xpath = ".//*[@id='routeSelection_DepartureStation-input']")
 	private WebElement departureWindow;
 
-	@FindBy(id = ".//*[@id='routeSelection_ArrivalStation-input']")
+	@FindBy(xpath = "//input[@id='routeSelection_DepartureStation-input']/../../../div[2]/span[1]")
+	private WebElement textInDepartureWindow;
+
+	@FindBy(xpath = ".//*[@id='routeSelection_ArrivalStation-input']")
 	private WebElement destinationWindow;
 
-	@FindBy(id = ".//*[@id='dateSelection_OutboundDate-datepicker']")
+	@FindBy(xpath = "//input[@id='routeSelection_ArrivalStation-input']/../../../div[2]/span[1]")
+	private WebElement textInDestinationWindow;
+
+	@FindBy(xpath = ".//*[@id='dateSelection_OutboundDate-datepicker']")
 	private WebElement outboundDateWindow;
 
-	@FindBy(id = ".//*[@id='dateSelection_IsReturnFlight-datepicker']")
+	@FindBy(xpath = ".//*[@id='dateSelection_IsReturnFlight-datepicker']")
 	private WebElement returnDateWindow;
 
-	@FindBy(id = ".//*[@id='desktop']/section/div[2]/div[2]/div/div/div[2]/div/div[1]/label")
+	@FindBy(xpath = ".//*[@id='desktop']/section/div[2]/div[2]/div/div/div[2]/div/div[1]/label")
 	private WebElement checkBox;
 
-	@FindBy(id = ".//*[@id='booking-passengers-input']")
+	@FindBy(xpath = ".//*[@id='booking-passengers-input']")
 	private WebElement bookingPassengersWindow;
 
-	@FindBy(id = ".//*[@id='desktop']/section/div[3]/div/button")
+	@FindBy(xpath = ".//*[@id='desktop']/section/div[2]/div[3]/div/div[1]/div/div")
+	private WebElement countOfBookingPassengers;
+	
+	@FindBy(xpath = ".//*[@id='desktop']/section/div[3]/div/button")
 	private WebElement searchButton;
+	
+	@FindBy(xpath = ".//*[@id='top']/div/div/div[3]/section/section/div/div[1]/div[1]/div/div[1]/div/h4")
+	private WebElement flight;
+	
 
 	public MainPage(WebDriver driver) {
 		super(driver);
 		PageFactory.initElements(driver, this);
 	}
 
-	public String textOfMainPageTitle(){
-		
-		String text=inscriptionOfPage.getText();
-		
+	public String textOfMainPageTitle() {
+
+		String text = inscriptionOfPage.getText();
 		return text;
+	}
+
+	public String selectCountryOfDeparture(String country) {
+
+		departureWindow.click();
+		departureWindow.sendKeys(country);
+		inscriptionOfPage.click();
+		String nameOfCountry = textInDepartureWindow.getAttribute("innerHTML");
+		return nameOfCountry;
+
+	}
+
+	public String selectCountryOfDestination(String country) {
+
+		destinationWindow.click();
+		destinationWindow.sendKeys(country);
+		inscriptionOfPage.click();
+		String nameOfCountry = textInDestinationWindow.getAttribute("innerHTML");
+		return nameOfCountry;
+
+	}
+
+	public String selectOutboundDate(String date) {
+
+		outboundDateWindow.click();
+		outboundDateWindow.clear();
+		outboundDateWindow.sendKeys(date);
+		String dateOfDeparture = outboundDateWindow.getAttribute("value");
+		return dateOfDeparture;
+
+	}
+
+	public boolean returnOnToUncheked() {
+
+		checkBox.click();
+		checkBox.click();
+		boolean check = checkBox.isSelected();
+		return check;
+
+	}
+
+	public String selectArrivakDate() {
+
+		returnDateWindow.click();
+		String dateOfDeparture = returnDateWindow.getAttribute("placeholder");
+		return dateOfDeparture;
+
+	}
+
+	public String bookingPassenger() {
+
+		bookingPassengersWindow.click();
+		String countOfPassengers = countOfBookingPassengers.getAttribute("innerHTML");
+		return countOfPassengers;
+
+	}
+
+	public String searchFlight() {
+
+		searchButton.click();
+		String flightNumber = flight.getAttribute("innerHTML");
+		return flightNumber;
 		
+
 	}
 }
--- src/test/java/by/epam/transavia/MainPageTest.java
+++ src/test/java/by/epam/transavia/MainPageTest.java
@@ -1,21 +1,74 @@
 package by.epam.transavia;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class MainPageTest extends BaseTest {
-	String textOnMainPage;
+	public static final String MAINPAGETITLE = "Where do you want to go?";
+	public static final String COUNTRYOFDEPARTURE = "Barcelona, Spain";
+	public static final String COUNTRYOFDESTINATION = "Munich, Germany";
+	public static final String OUTBOUNDDATE = "21 Aug 2017";
+
+	private MainPage mainPage;
+
 	@BeforeClass
 	public void openMainPage() {
 
-		MainPage mainPage = navigate(MainPage.URL);
-		 textOnMainPage=mainPage.textOfMainPageTitle();
+		mainPage = navigate(MainPage.URL);
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 	}
 
-	@Test
-	public void doLogin() {
+	@Test(priority = 1)
+	public void titleTest() {
+		String textOnMainPage = mainPage.textOfMainPageTitle();
+		Assert.assertEquals(textOnMainPage, MAINPAGETITLE);
+	}
 
-		System.out.println(textOnMainPage);
-		
+	@Test(priority = 2)
+	public void departureCountryTest() {
+		String departureCountry = mainPage.selectCountryOfDeparture(COUNTRYOFDEPARTURE);
+		Assert.assertEquals(departureCountry, COUNTRYOFDEPARTURE);
+	}
+
+	@Test(priority = 3)
+	public void destinationCountryTest() {
+		String destinationCountry = mainPage.selectCountryOfDestination(COUNTRYOFDESTINATION);
+		Assert.assertEquals(destinationCountry, COUNTRYOFDESTINATION);
+	}
+
+	@Test(priority = 4)
+	public void outboundDateTest() {
+		String outboundDate = mainPage.selectOutboundDate(OUTBOUNDDATE);
+		Assert.assertEquals(outboundDate, OUTBOUNDDATE);
+	}
+
+	@Test(priority = 5)
+	public void checkboxTest() {
+		boolean checked = mainPage.returnOnToUncheked();
+		Assert.assertEquals(checked, false);
+
+	}
+
+	@Test(priority = 6)
+	public void arrivalDateTest() {
+		String arrivalDate = mainPage.selectArrivakDate();
+		Assert.assertEquals(arrivalDate, "Single flight");
+	}
+
+	@Test(priority = 7)
+	public void bookingPassengerTest() {
+		String countOfPassengers = mainPage.bookingPassenger();
+		Assert.assertEquals(countOfPassengers, "1 Adult");
+	}
+	@Test(priority = 8)
+	public void flightTest() {
+		String flightNumber = mainPage.searchFlight();
+		System.out.println(flightNumber);
 	}
 }
